4ce2af7faeab48f89399bf62d150f79d0da68096
LordWalace <walaceestudante@gmail.com>
2025-11-27 15:28:23 -0300

    Biblioteca finalizada

--- HPS/main.c
+++ HPS/main.c
@@ -12,7 +12,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb_image_resize.h"
+#include "stb_image_resize2.h"
 
 #define IMG_PATH "./imagens/"
 #define MAX_FILENAME 100
